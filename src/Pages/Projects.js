d225c3a9ff260b8df0fc2ff6e84f43612220fd5c
Kutman7 <kutman.stel@gmail.com>
2022-03-28 16:49:55 +0200

    adding projects page and responsive

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -7,23 +7,30 @@ export default function Projects() {
         <div className="projects">
             <div className="title"> <FontAwesomeIcon icon={faSquareFull} color="blue" /> Projects </div>
             <div className="something">
-                <p style={{textAlign: "center"}}>I'm a paragraph. Click here to add your own text and edit me.
+                <p style={{ textAlign: "center" }}>I'm a paragraph. Click here to add your own text and edit me.
                     It’s easy. Just click “Edit Text” or double click me to add your
                     own content and make changes to the font. I’m a great place for
                     you to tell a story and let your users know a little more about you.
                 </p>
             </div>
             <div className="project-containers">
-                <div></div>
-            </div>
-            <div className="project-containers">
-                <div></div>
-            </div>
-            <div className="project-containers">
-                <div></div>
-            </div>
-            <div className="project-containers">
-                <div></div>
+                <div className='left-part'>
+                    <div>
+                        <div className='decorational-rectangle'></div>
+                        <div>
+                            <h5>Project name 01</h5>
+                            <h6>Role Title</h6>
+                        </div>
+
+                    </div>
+                    <p>I'm a paragraph. Click here to add your own text and edit me.
+                        It’s easy. Just click “Edit Text” or double click me to add
+                        your own content and make changes to the font. I’m a great
+                        place for you to tell a story and let your users know a
+                        little more about you.
+                    </p>
+                </div>
+                <img src="https://user-images.githubusercontent.com/73386100/160409992-bf913730-dd0e-463d-b93d-b5c97f522227.png" alt="my project" />
             </div>
         </div>
     )
